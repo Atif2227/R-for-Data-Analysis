64cf866710bca54b6e45cf3ccd0e69f23e1c5967
Atif Noorul Hasan <66054522+Atif2227@users.noreply.github.com>
2021-12-30 01:07:27 +0530

    Add files via upload

---
+++ Chapter 2 Data Types.R
@@ -0,0 +1,20 @@
+#Ch - 2 
+#Data Types
+-----------------------------
+
+# 1. Numeric
+num1 <- 3
+class(num1)
+
+# 2. Character
+char1 <- "Hello"
+class(char1)
+
+
+# 3. Logical
+log1 <- TRUE 
+class(log1)
+
+# 4. complex
+comp1 <- 5 + 7i
+class(comp1)
