1b12fe86409827a420e3e747aeca9b83c480c53b
Atif Noorul Hasan <66054522+Atif2227@users.noreply.github.com>
2022-01-03 20:58:52 +0530

    Add files via upload

---
+++ Chapter 3 Operators in R.R
@@ -0,0 +1,44 @@
+#Ch - 3 
+#Operators in R
+-----------------------------
+  
+# 1. Assignment operator
+# R has 3 assignment operators for assigning values to variables.
+# <-, -> and =, but generally <- is used
+  
+num1 = 1
+num2 <- 2
+3 -> num3
+
+# 2. Arithmetic Operators
+
+num1 + num2
+num2 - num1
+num1 / num2
+num1 * num2
+
+# 3. Relational Operators
+
+num1 < num2
+num2 > num2
+num1 == num2
+num1 != num2
+
+
+# 4. Logical Operators
+# lets store True and false in two variables.
+a = T
+b = F
+
+# And (&) operator 
+a & b
+b & a
+a & a
+b & b
+
+# Or (|) operator
+
+a|b
+b|a
+a|a
+b|b
